17081250cc652b159edf95a26c455009775ba5a3
reshmiranjani <50790684+reshmiranjani@users.noreply.github.com>
2019-05-19 23:03:41 +0530

    Create prgm9.c

---
+++ prgm9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int N,K,i,M[50],sum=0;
+scanf("%d %d",&N,&K);
+for(i=1;i<=5;i++)
+scanf("%d",&M[i]);
+for(i=1;i<=K;i++)
+sum=sum+i;
+printf("%d",sum);
+    return 0;
+}
