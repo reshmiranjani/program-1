8756f8b724dad4db346821c8fe47e7cd4388614e
reshmiranjani <50790684+reshmiranjani@users.noreply.github.com>
2019-05-20 11:50:59 +0530

    Update prgm3.c

--- prgm3.c
+++ prgm3.c
@@ -8,9 +8,9 @@ if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
 
  if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
  
-printf("Vowels"); 
+printf("Vowel"); 
 else
-printf("Consonants");
+printf("Consonant");
 else
 printf("invalid");
 
